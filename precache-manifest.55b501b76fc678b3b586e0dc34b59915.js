d92ccec3e05dc8e838b67d3ccf934060b48e1f53
Feng Ya <fengya04@126.com>
2018-12-21 18:30:44 +0800

    Updates

---
+++ precache-manifest.55b501b76fc678b3b586e0dc34b59915.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8eaf3627203353d4a5ef",
+    "url": "/vocabulary-builder/static/js/runtime~main.8eaf3627.js"
+  },
+  {
+    "revision": "b02d97dd7b530a004d36",
+    "url": "/vocabulary-builder/static/js/main.b02d97dd.chunk.js"
+  },
+  {
+    "revision": "98c70f3a5b2f111137bd",
+    "url": "/vocabulary-builder/static/js/1.98c70f3a.chunk.js"
+  },
+  {
+    "revision": "b02d97dd7b530a004d36",
+    "url": "/vocabulary-builder/static/css/main.2ba896c3.chunk.css"
+  },
+  {
+    "revision": "23dc89532e86c5e0b2bebf6ab9691069",
+    "url": "/vocabulary-builder/index.html"
+  }
+];
