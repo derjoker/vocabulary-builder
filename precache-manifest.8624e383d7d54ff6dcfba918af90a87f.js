834bb323e02e8dbe67f0fb4484126177da5d1a62
Feng Ya <fengya04@126.com>
2018-12-27 08:05:04 +0800

    Updates

---
+++ precache-manifest.8624e383d7d54ff6dcfba918af90a87f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8eaf3627203353d4a5ef",
+    "url": "/vocabulary-builder/static/js/runtime~main.8eaf3627.js"
+  },
+  {
+    "revision": "6994f2ebea8fa4806527",
+    "url": "/vocabulary-builder/static/js/main.6994f2eb.chunk.js"
+  },
+  {
+    "revision": "63b2084ddd636e4f1e53",
+    "url": "/vocabulary-builder/static/js/1.63b2084d.chunk.js"
+  },
+  {
+    "revision": "6994f2ebea8fa4806527",
+    "url": "/vocabulary-builder/static/css/main.6bd55e9e.chunk.css"
+  },
+  {
+    "revision": "b4493810e9ee2ef55bcc2b1c883255ca",
+    "url": "/vocabulary-builder/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/vocabulary-builder/precache-manifest.98c1aefc77b7ef8187e61f692a0c5f1c.js"
+  "/vocabulary-builder/precache-manifest.8624e383d7d54ff6dcfba918af90a87f.js"
 );
 
 workbox.clientsClaim();
